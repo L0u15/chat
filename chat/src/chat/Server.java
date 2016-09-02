741065c306b76f3b401f6c8f62b8d63cc9132a39
LouisL <louis.lallleau@gmail.com>
2016-09-02 15:36:09 +0200

    second commit

--- chat/src/chat/Server.java
+++ chat/src/chat/Server.java
@@ -10,6 +10,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.EOFException;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -27,6 +28,7 @@ public class Server extends JFrame {
     private JTextField userText;
     private JTextArea chatWindow;
     private ObjectOutputStream output;
+    private ObjectInputStream input;
     private ServerSocket server;
     private Socket connection;
 
@@ -50,21 +52,49 @@ public class Server extends JFrame {
         setSize(300, 150);
         setVisible(true);
     }
+
     // set up & run the server
-    public void startRunning(){
-        try{
-            server = new ServerSocket(6789,100);
-            while(true){
-                try{
-                    
-                }catch(EOFException eOFE){
+    public void startRunning() {
+        try {
+            server = new ServerSocket(6789, 100);
+            while (true) {
+                try {
+                    waitForConnection();
+                    setupStreams();
+                    whileChatting();
+                } catch (EOFException eOFE) {
                     showMessage("\n Server ended the connection!");
-                }finally{
+                } finally {
                     closeCrap();
                 }
             }
-        }catch(IOException iOE){
+        } catch (IOException iOE) {
             iOE.printStackTrace();
         }
+    }
+
+    //wait for a connection, then display connection information
+    private void waitForConnection() throws IOException {
+        showMessage("Waiting for someone to connect...\n");
+        connection = server.accept();
+        showMessage("Now connected to " + connection.getInetAddress().getHostName());
+    }
+
+//get stream to send and receive data
+    private void setupStreams() throws IOException {
+        output = new ObjectOutputStream(connection.getOutputStream());
+        output.flush();
+        input = new ObjectInputStream(connection.getInputStream());
+        showMessage("\n Streams are new setup!");
+    }
+
+//during the chat conversation
+    private void whileChatting() throws IOException{
+        String message = "You are now connected ! ";
+        sendMessage(message);
+        ableToType(true);
+        do{
+            // have a conversation
+        }while();
     }
 }
